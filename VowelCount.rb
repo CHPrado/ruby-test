d803a5ce45c0bce5a5731b0b589bfb0c61340479
Christian Prado <christianprado.contato@gmail.com>
2020-05-23 21:17:15 -0300

    Vowel Counter

---
+++ VowelCount.rb
@@ -0,0 +1,23 @@
+class VowelCount
+    def initialize(sentence)
+        count(sentence)
+    end
+
+    private
+
+    def count(sentence)
+        vowels = { "a"=> 0, "e"=> 0, "i"=> 0, "o"=> 0 , "u"=> 0 }
+
+        sentence.split("").each do |char|
+            vowels.each do |vowel, value|
+                puts char == vowel
+                if char == vowel
+                    vowels[vowel] += 1
+                end
+            end
+        end
+        puts vowels
+    end
+end
+
+VowelCount.new('O cachorro uiva faz auuuuu para a lua')
